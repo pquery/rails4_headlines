1054f71b433557eb1d5a09a780959647527e7cef
Peter Casanova <peter.casanova@comcast.net>
2015-06-07 14:06:33 -0400

    add users views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_prefix =>'auth'
+  resources :users
   resources :news do
   	member do 
   	  patch :publish
   	end
   end
+  
 
   root 'news#index'
 
